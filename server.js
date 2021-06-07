5f0ce27ab8a28a154ec34848a4ae4fa989819eac
Fernando Sanchez <fernando.sanchez@cartrack.com>
2021-06-07 20:15:30 +0800

    git init

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors')
+const bodyparser = require ('body-parser');
+const multiparty = require('connect-multiparty');
+const { error } = require('console');
+
+const path = require('path');
+
+const fs = require('fs');
+
+
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors())
+
+const MuiltiPartyMiddleware = multiparty({uploadDir:"./images"});
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+
+app.get('/', ( req, res) =>{
+    res.status(200).json(
+        {
+            message: "Data Ready And server Also"
+        }
+    )
+});
+
+app.use('/uploads', express.static('uploads'));
+
+app.post('/upload', MuiltiPartyMiddleware, (req, res) =>{
+    
+    var TempFile = req.files.upload;
+    var TempPathfile = TempFile.path;
+
+   const targetPathUrl = path.join(__dirname,"./uploads/"+TempFile.name);
+
+   console.log(TempFile, "aaaaaaaaaaaaaaaaaaaaa")
+
+   if(path.extname(TempFile.originalFilename).toLowerCase() === ".png" || ".jpg" || ".jpeg"){
+     
+    fs.rename(TempPathfile, targetPathUrl, err =>{
+        
+        res.status(200).json({
+            uploaded: true,
+            url: `http://localhost:5000/uploads/${TempFile.name}`
+        });
+
+        if(err) return console.log(err);
+    })
+   }
+})
+
+
+app.listen(PORT, console.log(`Server Started at PORT :${PORT}`))
